acd57fdbc4c833c949ab9981a75bcde3145a0888
ferdousbs-23 <ferdous.azad@brainstation-23.com>
2022-11-28 12:07:35 +0600

    remove node modules

--- server/server.js
+++ server/server.js
@@ -1,41 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
-const cors = require('cors');
-const  { Server } = require('socket.io');
-
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
 app.use(cors());
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
-    }
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
 io.on("connection", (socket) => {
-    console.log(`User:: ${socket.id} connected`);
+  console.log(`User Connected: ${socket.id}`);
 
-    socket.on("join_room", (data) => {
-        socket.join(data);
-        console.log(`User with ID: ${socket.id} joined room: ${data}`);
-    });
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
 
-    socket.on("send_message", (data) => {
-        console.log(data);
-    });
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
 
-    socket.on("recieve_message", (data) => {
-        socket.emit("recieve_message", data);
-    });
-    
-    socket.on("disconnect", () => {
-        console.log(`User:: ${socket.id} Disconnected`);
-    });
-})
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
 
 server.listen(3001, () => {
-    console.log('status:: running');
+  console.log("SERVER RUNNING");
 });
